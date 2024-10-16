d0d85efb90d8b7b56b0af016996ed192339b482f
mctuliomontoya <mctulio.2@gmail.com>
2024-10-15 18:01:56 -0700

    animate cards

--- src/sections/FeaturesCards.tsx
+++ src/sections/FeaturesCards.tsx
@@ -1,6 +1,7 @@
 import { twMerge } from "tailwind-merge";
 import { TextButton } from "../components/TextButton";
 import { Card } from "../components/Card";
+import { useEffect, useState } from "react";
 
 const cardData = [
   {
@@ -34,6 +35,20 @@ const cardData = [
 ];
 
 export const FeaturesCardsSection = () => {
+  const [selectedCardIndex, setSelectedCardIndex] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
+
+  useEffect(() => {
+    if (isHovered) return;
+    const timeout = setTimeout(() => {
+      setSelectedCardIndex((curr) =>
+        curr === cardData.length - 1 ? 0 : curr + 1
+      );
+    }, 3000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [selectedCardIndex, isHovered]);
   return (
     <section className="py-24 overflow-x-clip md:-mt-28">
       <div className="container">
@@ -43,31 +58,48 @@ export const FeaturesCardsSection = () => {
         <div className="mt-36 lg:mt-48 flex">
           <div className="flex flex-none gap-8">
             {cardData.map(({ image, title, description, color }, index) => (
-              <Card key={index} className="max-w-xs md:max-w-md" color={color}>
-                <div className="justify-center flex -mt-28">
-                  <div className="inline-flex relative">
-                    <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)] group-hover:bg-zinc-950/30 transition duration-300"></div>
-                    <img
-                      src={image}
-                      alt={title}
-                      className="size-40 group-hover:-translate-y-6 transition duration-300"
-                    />
+              <div
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                className="inline-flex transition-all duration-500"
+                style={{
+                  transform: `translateX(calc((-100% - 2rem) * ${selectedCardIndex}))`,
+                }}
+              >
+                <Card
+                  key={index}
+                  className="max-w-xs md:max-w-md"
+                  color={color}
+                >
+                  <div className="justify-center flex -mt-28">
+                    <div className="inline-flex relative">
+                      <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)] group-hover:bg-zinc-950/30 transition duration-300"></div>
+                      <img
+                        src={image}
+                        alt={title}
+                        className="size-40 group-hover:-translate-y-6 transition duration-300"
+                      />
+                    </div>
                   </div>
-                </div>
-                <h3 className="font-heading font-black text-3xl mt-12">
-                  {title}
-                </h3>
-                <p className="text-zinc-400 text-lg mt-4">{description}</p>
-              </Card>
+                  <h3 className="font-heading font-black text-3xl mt-12">
+                    {title}
+                  </h3>
+                  <p className="text-zinc-400 text-lg mt-4">{description}</p>
+                </Card>
+              </div>
             ))}
           </div>
         </div>
         <div className="flex justify-center mt-10">
           <div className="bg-zinc-950 inline-flex gap-4 p-2.5 rounded-full">
-            {cardData.map(({ title }) => (
+            {cardData.map(({ title }, cardIndex) => (
               <div
                 key={title}
-                className="size-2.5 bg-zinc-500 rounded-full cursor-pointer"
+                className={twMerge(
+                  "size-2.5 bg-zinc-500 rounded-full cursor-pointer",
+                  cardIndex === selectedCardIndex && "bg-zinc-300"
+                )}
+                onClick={() => setSelectedCardIndex(cardIndex)}
               ></div>
             ))}
           </div>
