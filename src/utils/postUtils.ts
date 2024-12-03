fa8c9f763d089dd77d10e3d00c042909297ac9b5
mctuliomontoya <mctulio.2@gmail.com>
2024-12-02 19:32:03 -0700

    feat: add blog posts & utils

--- src/utils/postUtils.ts
+++ src/utils/postUtils.ts
@@ -1,12 +1,13 @@
+// Function that returns a color word depending on the provided category
 export const getPostColorFromCategory = (category: string) => {
   switch (category) {
-    case "Sustainability":
-      return "lime";
-    case "Innovation":
-      return "cyan";
-    case "Security":
-      return "violet";
+    case 'Sustainability':
+      return 'lime'
+    case 'Innovation':
+      return 'cyan'
+    case 'Security':
+      return 'violet'
     default:
-      return "fuchsia";
+      return 'fuchsia'
   }
-};
+}
